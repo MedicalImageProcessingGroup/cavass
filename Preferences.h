900713a0d532b30933d6674c0756de36bf92bf23
George Grevera <ggrevera@sju.edu>
2023-02-03 17:42:28 -0500

    minor changes

--- Preferences.h
+++ Preferences.h
@@ -40,7 +40,10 @@ along with CAVASS.  If not, see <http://www.gnu.org/licenses/>.
  *  status/state of user preferences.  This class allows preferences
  *  to persist via loading and storing preferences in a wxConfig object.
  *  Changes are automatically stored (to the file ~/.CAVASS on Linux,
- *  ~/Library/Preferences/CAVASS\ Preferences on Mac OS).
+ *  ~/Library/Preferences/CAVASS\ Preferences on Mac OS, or
+ *  C:Users\ggrevera\AppData\Roaming\cavass.ini on Windows for example).
+ *  On Windows, one should avoid the Windows registry and use a file 
+ *  instead.
  */
 class Preferences {
   public:
